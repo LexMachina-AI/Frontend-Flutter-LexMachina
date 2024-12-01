b60ce0028911a2d65a853c2ceeb9c18c3d61e748
karthidreamr <karthidreamr@gmail.com>
2024-12-01 14:26:23 +0000

    firebase services initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv; 
 import 'pages/chat_page.dart';
 
+// void main() async {
+
+//   await Firebase.initializeApp();
+
+//   // Pass all uncaught "fatal" errors from the framework to Crashlytics
+//   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+//   runApp(MyApp());
+// }
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+
   await dotenv.dotenv.load();
   initializeDateFormatting().then((_) => runApp(const MyApp()));
   }
